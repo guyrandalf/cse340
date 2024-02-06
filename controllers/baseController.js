a091cce2b90e5b9e458e2ab3e18d2070b058803c
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2024-02-06 12:01:09 +0100

    baseController

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,7 +3,6 @@ const baseController = {};
 
 baseController.buildHome = async function (req, res) {
   const nav = await utilities.getNav();
-  res.render("index", { title: "Home", nav });
   res.render("index", { title: "Home", nav, errors: null });
 };
 
